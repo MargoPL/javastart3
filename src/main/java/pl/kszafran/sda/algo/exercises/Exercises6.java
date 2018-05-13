bdddac962ff6b5adf9e7118c1c93e3b4332ff71e
MargoPL\Mateusz Olewiński <mateusz.olewinski96@gmail.com>
2018-05-13 13:12:20 +0200

    Exercises6

--- src/main/java/pl/kszafran/sda/algo/exercises/Exercises6.java
+++ src/main/java/pl/kszafran/sda/algo/exercises/Exercises6.java
@@ -1,9 +1,8 @@
 package pl.kszafran.sda.algo.exercises;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import org.junit.platform.engine.support.hierarchical.Node;
+
+import java.util.*;
 
 /**
  * Zaimplementuj poniższe metody operujące na drzewie binarnym.
@@ -13,75 +12,139 @@ public class Exercises6 {
     /**
      * Przechodzi podane drzewo w kolejności pre-order i zwraca listę
      * elementów w kolejności takiej, w jakiej były napotkane.
-     *
+     * <p>
      * Uwaga: metodę należy zaimplementować z wykorzystaniem rekurencji.
      */
     public <T> List<T> traversePreOrder(SdaTree<T> tree) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        List<T> treePreOrder = new ArrayList<>();
+        treePreOrder.add(tree.getValue());
+        tree.getLeftChild().ifPresent(child -> treePreOrder.addAll(traversePreOrder(child)));
+        tree.getRightChild().ifPresent(child -> treePreOrder.addAll(traversePreOrder(child)));
+
+//        if (tree.getLeftChild().isPresent()) {
+//            treePreOrder.addAll(traversePreOrder(tree.getLeftChild().get()));
+//        }
+//        if (!tree.getRightChild().equals(Optional.empty())) {
+//            treePreOrder.addAll(traversePreOrder(tree.getRightChild().get()));
+//        }
+        return treePreOrder;
     }
 
     /**
      * Przechodzi podane drzewo w kolejności in-order i zwraca listę
      * elementów w kolejności takiej, w jakiej były napotkane.
-     *
+     * <p>
      * Uwaga: metodę należy zaimplementować z wykorzystaniem rekurencji.
      */
     public <T> List<T> traverseInOrder(SdaTree<T> tree) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        List<T> treeInOrder = new ArrayList<>();
+        tree.getLeftChild().ifPresent(child -> treeInOrder.addAll(traverseInOrder(child)));
+        treeInOrder.add(tree.getValue());
+        tree.getRightChild().ifPresent(child -> treeInOrder.addAll(traverseInOrder(child)));
+
+//        if (tree.getLeftChild().isPresent()) {
+//            treeInOrder.addAll(traverseInOrder((tree.getLeftChild().get())));
+//        }
+//        treePreOrder.add(tree.getValue());
+//        if (tree.getRightChild().isPresent()) {
+//            treeInOrder.addAll(traverseInOrder(tree.getRightChild().get()));
+//        }
+        return treeInOrder;
     }
 
     /**
      * Przechodzi podane drzewo w kolejności post-order i zwraca listę
      * elementów w kolejności takiej, w jakiej były napotkane.
-     *
+     * <p>
      * Uwaga: metodę należy zaimplementować z wykorzystaniem rekurencji.
      */
     public <T> List<T> traversePostOrder(SdaTree<T> tree) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        List<T> treePostOrder = new ArrayList<>();
+        tree.getLeftChild().ifPresent(child -> treePostOrder.addAll(traversePostOrder(child)));
+        tree.getRightChild().ifPresent(child -> treePostOrder.addAll(traversePostOrder(child)));
+        treePostOrder.add(tree.getValue());
+
+//        if (tree.getLeftChild().isPresent()) {
+//            treePostOrder.addAll(traversePostOrder((tree.getLeftChild().get())));
+//        }
+//        if (tree.getRightChild().isPresent()) {
+//            treePostOrder.addAll(traversePostOrder(tree.getRightChild().get()));
+//        }
+//        treePostOrder.add(tree.getValue());
+//
+        return treePostOrder;
     }
 
     /**
      * Funkcja działa tak samo jak traversePreOrder.
-     *
+     * <p>
      * Uwaga: metodę należy zaimplementować z wykorzystaniem stosu (bez rekurencji).
      */
     public <T> List<T> traversePreOrderIterative(SdaTree<T> tree) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Deque<SdaTree<T>> treePreOrderDeque = new ArrayDeque<>();
+        treePreOrderDeque.push(tree);
+        List<T> treePreOrderList = new ArrayList<>();
+
+        while (!treePreOrderDeque.isEmpty()) {
+            SdaTree<T> pop = treePreOrderDeque.pop();
+            treePreOrderList.add(pop.getValue());
+
+            pop.getRightChild().ifPresent(treePreOrderDeque::push);
+            pop.getLeftChild().ifPresent(treePreOrderDeque::push);
+        }
+        return treePreOrderList;
+
     }
 
     /**
      * Przechodzi podane drzewo w kolejności level-order i zwraca listę
      * elementów w kolejności takiej, w jakiej były napotkane.
-     *
+     * <p>
      * Podpowiedź: implementacja jest bardzo podobna do traversePreOrderIterative,
      * ale zamiast stosu wykorzystuje inną strukturę danych.
      */
     public <T> List<T> traverseLevelOrder(SdaTree<T> tree) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        Deque<SdaTree<T>> treeLevelOrderDeque = new ArrayDeque<>();
+        treeLevelOrderDeque.offer(tree);
+        List<T> treeLevelOrderList = new ArrayList<>();
+
+        while (!treeLevelOrderDeque.isEmpty()) {
+
+            SdaTree<T> poll = treeLevelOrderDeque.poll();
+            treeLevelOrderList.add(poll.getValue());
+
+            poll.getLeftChild().ifPresent(treeLevelOrderDeque::offer);
+            poll.getRightChild().ifPresent(treeLevelOrderDeque::offer);
+        }
+        return treeLevelOrderList;
     }
 
     /**
      * Funkcja zwraca liczbę liści w podanym drzewie.
      */
     public int countLeaves(SdaTree<?> tree) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (!tree.getLeftChild().isPresent() && !tree.getRightChild().isPresent()) return 1;
+
+        
+
     }
 
     /**
      * Tworzy drzewo binarne na podstawie podanego tekstu.
-     *
+     * <p>
      * Tekst zawiera tyle linijek, ile poziomów ma drzewo.
      * Każda linijka zawiera wartości węzłów na odpowiednim poziomie rozdzielone spacjami, po kolei,
      * czyli każda linijka zawiera dwa razy więcej wartości niż poprzednia.
      * Wartość "-" oznacza, że węzeł na danej pozycji nie istnieje.
-     *
+     * <p>
      * Np. drzewo ze slajdów przedstawione byłoby jako "F\nB G\nA D - I\n- - C E - - H -",
      * czyli zapisując w wielu liniach:
      * F
      * B G
      * A D - I
      * - - C E - - H -
-     *
+     * <p>
      * Uwaga: nie należy modyfikować klas SdaTree i SdaTreeImpl.
      *
      * @throws IllegalArgumentException jeśli któraś z linijek zawiera nieprawidłową ilość wartości
@@ -98,18 +161,18 @@ public class Exercises6 {
 
     /**
      * Tworzy drzewo binarne na podstawie podanego tekstu.
-     *
+     * <p>
      * Każda linijka zawiera informację o parze rodzic-dziecko.
      * Format każdej linijki wygląda następująco:
-     *
+     * <p>
      * left(rodzic)=dziecko
-     *
+     * <p>
      * lub
-     *
+     * <p>
      * right(rodzic)=dziecko
-     *
+     * <p>
      * dla lewego i prawego dziecka odpowiednio.
-     *
+     * <p>
      * Uwaga: nie należy modyfikować klas SdaTree i SdaTreeImpl.
      *
      * @throws IllegalArgumentException jeśli któraś z linijek jest niezgodna z powyższym formatem
@@ -120,7 +183,7 @@ public class Exercises6 {
 
     /**
      * Funkcja oblicza wysokość drzewa.
-     *
+     * <p>
      * Przypomnienie: wysokość drzewa składającego się jedynie z korzenia to 0.
      */
     public int calcHeight(SdaTree<?> tree) {
