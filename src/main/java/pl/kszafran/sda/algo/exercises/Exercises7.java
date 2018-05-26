91bb8477819ec77197db55d022ec176f1ec474fd
MargoPL\Mateusz Olewiński <mateusz.olewinski96@gmail.com>
2018-05-26 15:52:22 +0200

    Ex.7

--- src/main/java/pl/kszafran/sda/algo/exercises/Exercises7.java
+++ src/main/java/pl/kszafran/sda/algo/exercises/Exercises7.java
@@ -14,7 +14,16 @@ public class Exercises7 {
      * ma wartość większą lub równą wartości swoich dzieci.
      */
     public boolean isHeap(int[] array) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        int parent = array.length / 2;
+        for (int i = 0; i < parent; i++) {
+            int left = (2 * i) + 1;
+            int right = (2 * i) + 2;
+            if (array[i] < array[left] || (right < array.length && array[i] < array[right])) {
+                return false;
+            }
+
+        }
+        return true;
     }
 
     public <T extends Comparable<T>> SdaHeap<T> createHeap(T[] heap, int capacity) {
@@ -61,7 +70,32 @@ public class Exercises7 {
 
         @Override
         public void push(T element) {
-            throw new UnsupportedOperationException("Not implemented yet");
+            if (size == heap.length) throw new IllegalStateException();
+//            A[A.length] = v
+//            i = A.length-1
+//            while (i > 1 && A[parent(i)] < A[i])
+//            swap(A[i], A[parent(i)])
+
+            heap[size] = element;
+            size++;
+
+            int parent = heap.length / 2;
+            int left = 2 * parent + 1;
+            int right = 2 * parent + 2;
+
+            if (heap[parent].compareTo(heap[left]) < 0) {
+                swap(parent, left);
+            }
+            if (heap[parent].compareTo(heap[right]) < 0) {
+                swap(parent, right);
+            }
+        }
+
+        void swap(int i, int j) {
+            T temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
+
         }
 
         @Override
@@ -87,7 +121,7 @@ public class Exercises7 {
 
         /**
          * Wstawia nowy element do drzewa BST.
-         *
+         * <p>
          * Jeśli element o takiej samej wartości już znajduje się w drzewie,
          * zostaje zastąpiony przez nowy element.
          */
@@ -95,22 +129,22 @@ public class Exercises7 {
 
         /**
          * Zwraca true, jeśli podany element znajduje się w drzewie.
-         *
+         * <p>
          * Uwaga: elementy należy porównywać poprzez .compareTo(..), nie .equals(..).
          */
         boolean contains(T element);
 
         /**
          * Usuwa element z drzewa BST (jeśli taki istnieje).
-         *
+         * <p>
          * Podpowiedź: należy rozpatrzyć trzy przypadki:
          * - usuwany węzeł nie ma dzieci
          * - usuwany węzeł ma jedno dziecko
          * - usuwany węzeł ma dwoje dzieci
-         *
+         * <p>
          * Uwaga: zauważ, że nasza implementacja Node nie przechowuje referencji na rodzica (parent),
          * więc nie każde rozwiazanie znalezione w Internecie się dla nas nadaje.
-         *
+         * <p>
          * Podpowiedź: ta stronka może się tutaj bardziej przydać niż Wikipedia:
          * https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
          */
@@ -135,7 +169,20 @@ public class Exercises7 {
 
         @Override
         public void insert(T element) {
-            throw new UnsupportedOperationException("Not implemented yet");
+            if (root == null) {
+                root = new Node(element, null, null);
+            }else insertNewValue(root, element);
+
+        }
+
+        private void insertNewValue(Node root, T element) {
+            if (element.compareTo(root.value) < 0) {
+                element.compareTo(root.right.value);
+            } else {
+                insert(element);
+            }
+
+
         }
 
         @Override
